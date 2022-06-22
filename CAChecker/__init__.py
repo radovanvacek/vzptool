515986e2954b88b10cdb9b5a4ca4176fa0c78e7a
radovan <radovan.vacek@insighti.com>
2022-06-22 12:25:01 +0200

    added connection error handling allinged variable naming convention

--- CAChecker/__init__.py
+++ CAChecker/__init__.py
@@ -20,22 +20,24 @@ class CAInfoUpdater(threading.Thread):
     def __init__(self, ipv4, port, data_dir):
         super().__init__()
         self._db = None
-        self.ipv4 = ipv4
-        self.port = int(port)
+        self._ipv4 = ipv4
+        self._port = int(port)
         self._data_dir = data_dir
 
     def run(self, n=None):
-        print("{}: collecting CA information for {}:{}".format(threading.current_thread().ident, self.ipv4, self.port))
+        print(
+            "{}: collecting CA information for {}:{}".format(threading.current_thread().ident, self._ipv4, self._port))
         self._db = database.Database(self._data_dir)
 
         try:
-            cert = ssl.get_server_certificate((self.ipv4, self.port), ssl.PROTOCOL_SSLv23)
+            cert = ssl.get_server_certificate((self._ipv4, self._port), ssl.PROTOCOL_SSLv23)
             parsed = x509.load_pem_x509_certificate(str.encode(cert), default_backend())
             issuer = parsed.issuer.rfc4514_string()
             authority_key_identifier = parsed.extensions.get_extension_for_oid(ExtensionOID.AUTHORITY_KEY_IDENTIFIER)
-            self._db.update_cert_info(authority_key_identifier.value.key_identifier.hex(), issuer, self.ipv4, self.port)
+            self._db.update_cert_info(authority_key_identifier.value.key_identifier.hex(), issuer, self._ipv4,
+                                      self._port)
         except ssl.SSLError as err:
             print(err)
         except ConnectionError as err:
-            print('Connection error to Host {} on port {}'.format(self.ipv4, self.port))
+            print('Connection error to Host {} on port {}'.format(self._ipv4, self._port))
             print(err)
--- HTTPSRedirectChecker/__init__.py
+++ HTTPSRedirectChecker/__init__.py
@@ -23,7 +23,7 @@ class HTTSPRedirectChecker(threading.Thread):
         try:
             response = requests.get(self._url, allow_redirects=False)
         except (ConnectionError, NewConnectionError, MaxRetryError) as err:
-            print('Connection error to Host {} on port {}'.format(self.ipv4, self.port))
+            print('Connection error to Host {} on port {}'.format(self._ipv4, self._port))
             print(err)
         if response.status_code in (301, 302):
             self._db.update_redirect_status(self._ipv4, self._port, str(response.content), True)
