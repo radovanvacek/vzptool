0c43c71008db874d20cbf0a3428b22af9132a37e
radovan <radovan.vacek@insighti.com>
2022-06-23 16:25:18 +0200

    testing a hack from https://github.com/psf/requests/issues/1418#issuecomment-19285850

--- APIOrWebChecker/__init__.py
+++ APIOrWebChecker/__init__.py
@@ -1,11 +1,27 @@
 import logging
+import ssl
 import threading
 
-import requests
+from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
+from requests.packages.urllib3.poolmanager import PoolManager
 from urllib3.exceptions import NewConnectionError, MaxRetryError
 
 import database
+
+
+class MyAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block):
+        self.poolmanager = PoolManager(num_pools=connections,
+                                       maxsize=maxsize,
+                                       block=block,
+                                       ssl_version=ssl.PROTOCOL_TLSv1)
+
+
+import requests
+
+s = requests.Session()
+s.mount('https://', MyAdapter())
 
 
 class APIOrWebChecker(threading.Thread):
